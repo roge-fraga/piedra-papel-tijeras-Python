8cb2a3ecd8c4b6a4a642abc6f48a91a989429c18
Rogelio <rogeliofragaa@outlook.com>
2026-01-23 18:02:37 -0300

    Juego papel, piedra o tijeras de dos jugadores en python

---
+++ piedra-papel-tijeras.py
@@ -0,0 +1,16 @@
+nombre1 = input("¿Cómo se llamará el jugador 1?")
+nombre2 = input("¿Cómo se llamará el jugador 2?")
+
+jugador1 = input(f"¡Hola {nombre1}! ¿Qué eliges? ¿Piedra, papel o tijeras?: ")
+jugador2 = input(f"¡Hola {nombre2}! ¿Qué eliges? ¿Piedra, papel o tijeras?: ")
+
+condicion1 = jugador1 == "piedra" and jugador2 == "tijeras"
+condicion2 = jugador1 == "papel" and jugador2 == "piedra"
+condicion3 = jugador1 == "tijeras" and jugador2 == "papel"
+
+if jugador1 == jugador2:
+    print("¡Ha sigo un empate!")
+elif condicion1 or condicion2 or condicion3:
+    print(f"Ha ganado {nombre1}")
+else:
+    print(f"Ha ganado el {nombre2}")
